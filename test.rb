657f21fb1c9fa4bd4a5fadc21c3624cc1998b1a4
chienleow <chienleow1@gmail.com>
2021-06-10 00:33:12 -0400

    ruby method definitions

---
+++ test.rb
@@ -0,0 +1,10 @@
+# Ruby Methods
+# Method is a block of code we write to perform a specific task for us
+# - you can call them from other places inside your program
+# - we can give methods information, they can give us information back
+
+def sayhi
+    puts "Hello User"
+end
+
+sayhi
